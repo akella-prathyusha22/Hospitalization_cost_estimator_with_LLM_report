8ae6e3dcbf5bbaad26a1cc264cd85a500050daf0
akella-prathyusha22 <akella.prathyusha22@gmail.com>
2026-08-04 15:49:34 +0530

    Fixed date format

--- app.py
+++ app.py
@@ -30,12 +30,15 @@ with st.form("patient_form"):
     heart_issues = st.selectbox("Any Heart issues?", ["yes", "no"])
     num_of_major_surgeries = st.number_input("Number of Major Surgeries", min_value=0, max_value=50, value=2)
 
+    # Convert date to a string
+    dob_str = dob.strftime("%Y-%m-%d")
+    
     submitted = st.form_submit_button("Estimate Cost")
 
 if submitted:
     new_patient = pd.DataFrame([{
         #"age": age,
-        "dob" : dob,
+        "dob" : dob_str,
         "height": height,
         "weight": weight,
         "children": num_of_children,
